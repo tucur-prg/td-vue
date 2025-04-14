c567badb5529b26b5df06356f16ac2640ab9f1eb
tucur-prg <tucur.prg+github@gmail.com>
2025-04-14 17:04:40 +0900

    prettier settings

--- scripts/pinia/vite.config.ts
+++ scripts/pinia/vite.config.ts
@@ -5,17 +5,14 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    vue()
-
-  ],
+  plugins: [vue()],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
-    },
+    }
   },
   server: {
-    host: "0.0.0.0",
+    host: '0.0.0.0',
     port: 4000
   }
 })
--- scripts/vuetify/vite.config.ts
+++ scripts/vuetify/vite.config.ts
@@ -15,10 +15,10 @@ export default defineConfig({
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
-    },
+    }
   },
   server: {
-    host: "0.0.0.0",
+    host: '0.0.0.0',
     port: 4000
   }
 })

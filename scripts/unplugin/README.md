039141695483f2c866275cf162408bf23ad5d0a7
白崎紀充 <magnet.0111.eng@gmail.com>
2025-04-14 17:04:56 +0900

    add unplugin

---
+++ scripts/unplugin/README.md
@@ -0,0 +1,18 @@
+Unplugin Example
+----
+
+
+
+# unplugin-vue-router
+
+https://github.com/posva/unplugin-vue-router
+
+https://uvr.esm.is/introduction
+
+[Vue.js でファイルベースルーティングを実現する「Unplugin Vue Router」の紹介](https://zenn.dev/comm_vue_nuxt/articles/2024-08-13-unplugin-vue-router)  
+
+## 404 Not Found
+
+https://uvr.esm.is/guide/file-based-routing.html#catch-all-404-not-found-route
+
+
---
+++ scripts/unplugin/src/pages/index.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+import { RouterLink } from 'vue-router'
+</script>
+
+<template>
+  <h1>Home</h1>
+  <div>
+    <RouterLink class="nav-link" to="/users/1">Go to User 1</RouterLink>
+    <RouterLink class="nav-link" to="/users/2">Go to User 2</RouterLink>
+  </div>
+</template>
+
+<style scoped>
+.nav-link {
+  padding: 0 6px;
+}
+.nav-link + .nav-link {
+  border-left: 1px solid #333;
+}
+</style>
